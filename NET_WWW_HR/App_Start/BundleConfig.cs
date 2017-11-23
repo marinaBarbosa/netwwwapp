196f93baa3ce8a156d76c1ad99216c2e7daba945
Marina Barbosa <marinab@ipvc.pt>
2017-11-23 03:39:24 +0100

    [1] Layout + Login
    - Next Task -> Fix layout + Login

--- NET_WWW_HR/App_Start/BundleConfig.cs
+++ NET_WWW_HR/App_Start/BundleConfig.cs
@@ -21,11 +21,12 @@ namespace NET_WWW_HR
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/clean-blog.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/clean-blog.min.css"));
         }
     }
 }
